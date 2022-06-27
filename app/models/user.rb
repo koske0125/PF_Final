4293b5a0bf55890d67cd6316a75e424a907615ef
koske0125 <kousuke.0125.dm@gmail.com>
2022-06-27 02:20:41 +0000

    [Add]ユーザーモデルにバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ApplicationRecord
   has_one_attached :profile_image
 
   enum is_published: { closed: 0, limited: 1, opened: 2 }
+
+  validates :name, presence: true
+  validates :summoner_name, presence: true
+  validates :is_published, presence: true
 end
