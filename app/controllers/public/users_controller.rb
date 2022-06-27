03f71b52306dd5d57fee2c2cfc0f8adb0ac3e007
koske0125 <kousuke.0125.dm@gmail.com>
2022-06-27 03:49:22 +0000

    [Add]ユーザー情報編集機能の追加

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,6 +1,28 @@
 class Public::UsersController < ApplicationController
+  before_action :authenticate_user!
 
   def show
     @user = User.find(current_user.id)
   end
+
+  def edit
+    @user = User.find(current_user.id)
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to public_user_path(@user.id), success: "プロフィールを更新しました"
+    else
+      render "edit"
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:profile_image, :name, :summoner_name, :email, :is_published, :password)
+  end
+
 end
