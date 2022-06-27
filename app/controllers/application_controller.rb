a0fc2d7921e7d95114e938c5caf162c341bae304
koske0125 <kousuke.0125.dm@gmail.com>
2022-06-27 02:16:54 +0000

    [Add]ユーザー登録時のストロングパラメーターを設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :warning, :danger #flashメッセージのキーを追加
+  before_action :configure_permitted_parameters, if: :devise_controller? #deviseストロングパラメーターの設定
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :summoner_name, :is_published, :authenticate, :is_deleted])
+  end
+
 end
