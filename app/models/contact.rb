7e0af2fd6b7131be968e22fc14d694487d258fa9
koske0125 <kousuke.0125.dm@gmail.com>
2022-07-06 09:29:12 +0000

    [Add]送信フォームの実装

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,7 @@
 class Contact < ApplicationRecord
 
   validates :name, presence: true, length: { in: 1..20 }
-  validates :email, presence: true, format: { with: /\A[\w+-.]+@[a-z\d-.]+.[a-z]+\z/i }, length: { maximum: 150 }
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, length: { maximum: 150 }
   validates :subject, presence: true, length: { in: 1..100 }
   validates :message, presence: true, length: { in: 1..1000 }
 
