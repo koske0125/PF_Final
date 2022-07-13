632f18a0d6247b3b44b8959173229854555cd4fa
koske0125 <kousuke.0125.dm@gmail.com>
2022-07-13 07:14:58 +0000

    [Add]ユーザー側boardsコントローラーの追加

--- app/controllers/public/boards_controller.rb
+++ app/controllers/public/boards_controller.rb
@@ -1,2 +1,28 @@
 class Public::BoardsController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
+
+  def index
+    @boards = Board.all.order(updated_at: :desc)
+    @board = Board.new
+  end
+
+  def create
+    @board = current_user.boards.build(board_params)
+    if @board.save
+      redirect_to public_board_path(@board), success: "投稿しました"
+    else
+      render "index"
+    end
+  end
+
+  def show
+    @board = Board.find(params[:id])
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:title, :body)
+  end
+
 end
