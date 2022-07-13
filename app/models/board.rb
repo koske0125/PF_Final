3c68b556e7da29a759a1d0924714df726ce1a156
koske0125 <kousuke.0125.dm@gmail.com>
2022-07-13 06:56:36 +0000

    [Add]Boardモデルにバリデーションを追加/Userモデルとのアソシエーションを設定

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,2 +1,6 @@
 class Board < ApplicationRecord
+  belongs_to :user
+
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :body, presence: true, length: { maximum: 1000 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ApplicationRecord
   has_many :followers, through: :reverse_of_relationships, source: :following
   has_many :entries, dependent: :destroy
   has_many :messages, dependent: :destroy
+  has_many :boards, dependent: :destroy
 
   has_one_attached :profile_image
 
