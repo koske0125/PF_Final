ee33858c8059edf46367147fbc2c1ce0e581a15d
koske0125 <kousuke.0125.dm@gmail.com>
2022-07-14 07:56:53 +0000

    [Add]User/Board/BoardCommentのアソシエーション/バリデーションの追加

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,5 +1,6 @@
 class Board < ApplicationRecord
   belongs_to :user
+  has_many :board_comments, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 50 }
   validates :body, presence: true, length: { maximum: 1000 }
--- app/models/board_comment.rb
+++ app/models/board_comment.rb
@@ -1,2 +1,8 @@
 class BoardComment < ApplicationRecord
+  belongs_to :user
+  belongs_to :board
+
+  validates :body, presence: true, length: { maximum: 1000 }
+
+  has_one_attached :board_comments_image
 end
