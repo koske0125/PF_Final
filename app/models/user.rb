0d7668e2738dcb875e13c396f6d23fff4bf1ec44
koske0125 <kousuke.0125.dm@gmail.com>
2022-07-15 05:28:38 +0000

    [Fix]Userモデルのアソシエーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,11 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follower
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: :follower_id
   has_many :followers, through: :reverse_of_relationships, source: :following
-  has_many :entries, dependent: :destroy
-  has_many :messages, dependent: :destroy
   has_many :boards, dependent: :destroy
   has_many :board_comments, dependent: :destroy
+  has_many :entries, dependent: :destroy
+  has_many :rooms, through: :entries
+  has_many :messages, dependent: :destroy
 
   has_one_attached :profile_image
 
