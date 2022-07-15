98588039af0922319b772ea996222052529adfb8
koske0125 <kousuke.0125.dm@gmail.com>
2022-07-15 07:39:08 +0000

    [Add]roomコントローラーにcreateアクションを定義

--- app/controllers/public/rooms_controller.rb
+++ app/controllers/public/rooms_controller.rb
@@ -9,7 +9,15 @@ class Public::RoomsController < ApplicationController
   end
 
   def create
+    @room = Room.new(room_params)
+    @room.save
+    redirect_to public_room_messages_path(@room)
+  end
 
+  private
+
+  def room_params
+    params.require(:room).permit(user_ids: [])
   end
 
 end
