db2a246841a177d4dd8de4933dfd4defc41f6861
koske0125 <kousuke.0125.dm@gmail.com>
2022-07-15 05:34:55 +0000

    [Fix]Roomモデルのアソシエーションを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
   has_many :boards, dependent: :destroy
   has_many :board_comments, dependent: :destroy
   has_many :entries, dependent: :destroy
-  has_many :rooms, through: :entries
+  has_many :rooms, through: :entries, dependent: :destroy
   has_many :messages, dependent: :destroy
 
   has_one_attached :profile_image
