1ffa128911a9a469dd98c25e8974f7f25ab4390d
koske0125 <kousuke.0125.dm@gmail.com>
2022-07-16 05:32:54 +0000

    [Add]DM機能の完成

--- Gemfile
+++ Gemfile
@@ -58,6 +58,7 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 gem "enum_help"
 gem "kaminari"
+gem 'pry-rails'
 
 gem 'dotenv-rails'
 group :production do
--- app/controllers/public/rooms_controller.rb
+++ app/controllers/public/rooms_controller.rb
@@ -10,10 +10,29 @@ class Public::RoomsController < ApplicationController
   end
 
   def create
-    @room = Room.new(room_params)
-    @room.name = current_user.name
-    @room.save
-    redirect_to public_room_messages_path(@room)
+    #すでに共通のルームが存在するかを確認する
+    @self = User.find(current_user.id)
+    @partner = User.find(params[:partner_id])
+    @self_entries = Entry.where(user_id: @self.id)
+    @partner_entries = Entry.where(user_id: @partner)
+
+    @self_entries.each do |self_entry|
+      @partner_entries.each do |partner_entry|
+        if self_entry.room_id == partner_entry.room_id then
+          @room_id = self_entry.room_id
+        end
+      end
+    end
+
+    if @room_id
+      @room = Room.find(@room_id)
+      redirect_to public_room_messages_path(@room)
+    else
+      @room = Room.new(room_params)
+      @room.name = current_user.name
+      @room.save
+      redirect_to public_room_messages_path(@room)
+    end
   end
 
   private
