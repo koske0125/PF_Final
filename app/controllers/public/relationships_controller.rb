9acf3175bf2bf83dd0efd3279615c456a765f20a
koske0125 <kousuke.0125.dm@gmail.com>
2022-07-17 04:55:13 +0000

    [Add]timestampsを日本時間に修正

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -2,6 +2,7 @@ class Public::RelationshipsController < ApplicationController
   before_action :authenticate_user!
 
   def create
+    @friends = Friend.all.order(updated_at: :desc).page(params[:page]).per(6)
     following = current_user.relationships.build(follower_id: params[:user_id])
     following.save
     redirect_to request.referrer || public_path
