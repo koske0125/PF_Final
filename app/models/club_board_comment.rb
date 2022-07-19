1637304150f6e9f2c4f032dc6366d38d16c2ee4b
koske0125 <kousuke.0125.dm@gmail.com>
2022-07-19 13:00:01 +0000

    [Fix]サークル機能のdependet: :destroyを修正

--- app/models/club_board_comment.rb
+++ app/models/club_board_comment.rb
@@ -1,6 +1,6 @@
 class ClubBoardComment < ApplicationRecord
-  belongs_to :user, dependent: :destroy
-  belongs_to :club_board, dependent: :destroy
+  belongs_to :user
+  belongs_to :club_board
 
   validates :body, presence: true, length: { maximum: 1000}
 
