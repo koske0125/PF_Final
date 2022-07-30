a293648ccf2f238a90a23b516ceef2a9212061a6
koske0125 <kousuke.0125.dm@gmail.com>
2022-07-30 08:06:14 +0000

    [ADD]管理者サークル一覧画面の実装

--- app/controllers/admin/clubs_controller.rb
+++ app/controllers/admin/clubs_controller.rb
@@ -1,2 +1,34 @@
 class Admin::ClubsController < ApplicationController
+  before_action :authenticate_admin!
+
+  def index
+    @clubs = Club.all.order(updated_at: :desc).page(params[:page]).per(20)
+  end
+
+  def show
+    @club = Club.find(params[:id])
+  end
+
+  def edit
+    @club = Club.find(params[:id])
+  end
+
+  def update
+    @club = Club.find(params[:id])
+    @club.update(club_params)
+    redirect_to admin_club_path(@club), success: "サークル情報を更新しました"
+  end
+
+  def destroy
+    @club = Club.find(params[:id])
+    @club.destroy
+    redirect_to admin_clubs_path, success: "サークルを削除しました"
+  end
+
+  private
+
+  def club_params
+    params.require(:club).permit(:name, :introduction)
+  end
+
 end
