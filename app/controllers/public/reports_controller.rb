7fb480c22765cd1daa1f156525738a2474e8cf4a
koske0125 <kousuke.0125.dm@gmail.com>
2022-08-17 08:16:59 +0000

    [Add]public/reportにcreateアクションを追加

--- app/controllers/public/reports_controller.rb
+++ app/controllers/public/reports_controller.rb
@@ -1,2 +1,14 @@
 class Public::ReportsController < ApplicationController
+  def create
+    @report = Report.new
+    @report.save(report_params)
+    redirect_to :back, success: "報告が完了しました"
+  end
+
+  private
+
+  def report_params
+    params.require(:report).permit(:genre,:address,:status)
+  end
+
 end
